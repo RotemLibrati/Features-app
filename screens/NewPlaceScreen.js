11d5392d1a038e791953184a6fe8f87460bf4c45
RotemLibrati <rotemlibrati1@gmail.com>
2021-09-29 06:37:56 +0300

    Add option to add new place, using at camera and add title to place.

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,12 +1,62 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TextInput, ScrollView, Button } from "react-native";
+import Color from "../constants/Color";
+import { useDispatch } from "react-redux";
+import * as placesActions from '../store/places-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const NewPlaceScreen = props => {
-    return ( <View>
-        <Text>NewPlaceScreen</Text>
-    </View>
+    const [titleValue, setTitleValue] = useState('');
+    const [selectedImage, setSelectedImage] = useState();
+    const dispatch = useDispatch();
+    const titleChangeHandler = text => {
+        setTitleValue(text);
+    };
+    const imageTakenHandler = imagePath => {
+        setSelectedImage(imagePath);
+    }
+    const savePlaceHandler = () => {
+        dispatch(placesActions.addPlace(titleValue, selectedImage));
+        props.navigation.goBack(); // return me back
+    };
+
+    return (
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput}
+                    onChangeText={titleChangeHandler}
+                    value={titleValue}
+                />
+                <ImagePicker onImageTaken={imageTakenHandler}/>
+                <Button title="Save Place"
+                    color={Color.primary}
+                    onPress={savePlaceHandler}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
-const styles = StyleSheet.create({});
+NewPlaceScreen.navigationOptions = {
+    headerTitle: 'Add Place'
+};
+
+const styles = StyleSheet.create({
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+});
 
 export default NewPlaceScreen;
