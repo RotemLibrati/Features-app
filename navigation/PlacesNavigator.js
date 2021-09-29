8d5e363fbfd03c0931e493b0c98b96faf8815b0f
RotemLibrati <rotemlibrati1@gmail.com>
2021-09-29 06:41:52 +0300

    navigation

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { ColorPropType, Platform } from 'react-native';
-import PlacesListScreen from '../screens/PlacesListScreen';
-import PlaceDetailScreen from '../screens/PlaceDetailScreen';
-import NewPlaceScreen from '../screens/NewPlaceScreen';
+import { Platform } from 'react-native';
 import MapScreen from '../screens/MapScreen';
-import Colors from '../constants/Colors';
+import NewPlaceScreen from '../screens/NewPlaceScreen';
+import PlaceDetailScreen from '../screens/PlaceDetailScreen';
+import PlacesListScreen from '../screens/PlacesListScreen';
+import Color from '../constants/Color';
 const PlacesNavigator = createStackNavigator({
     Places: PlacesListScreen,
     PlaceDetail: PlaceDetailScreen,
@@ -14,10 +15,9 @@ const PlacesNavigator = createStackNavigator({
 },{
     defaultNavigationOptions:{
         headerStyle:{
-            backgroundColor: Platform.OS === 'anroid' ?  Colors.primary : ''
+            backgroundColor: Platform.OS === 'anroid' ?  Color.primary : ''
         },
-        headerTintColor: Platform.OS === 'anroid' ? 'white' : Colors.primary
+        headerTintColor: Platform.OS === 'anroid' ? 'white' : Color.primary
     }
 });
-
 export default createAppContainer(PlacesNavigator);
