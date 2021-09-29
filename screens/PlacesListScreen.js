5591675edda9e80c8374d77661d79638f8ad8b20
RotemLibrati <rotemlibrati1@gmail.com>
2021-09-29 06:38:38 +0300

    Update list of places

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,11 +1,45 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, Platform, FlatList } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButton";
+import { useSelector } from "react-redux";
+import PlaceItem from "../components/PlaceItem.";
 
 const PlacesListScreen = props => {
-    return ( <View>
-        <Text>Places List Screen</Text>
-    </View>
+    const places = useSelector(state => state.places.places);
+    return (
+        <FlatList
+            data={places}
+            keyExtractor={item => item.id}
+            renderItem={itemData => <PlaceItem
+                image={itemData.item.imageUri}
+                title={itemData.item.title}
+                address={null}
+                onSelect={() => {
+                    props.navigation.navigate('PlaceDetail', {
+                        placeTitle: itemData.item.title,
+                        placeId: itemData.item.id
+                    })
+                }}
+            />}
+        />
     );
 };
+
+PlacesListScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'All Places',
+        headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item
+                title='Add Place'
+                iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+                onPress={() => {
+                    navData.navigation.navigate('NewPlace')
+                }}
+            />
+        </HeaderButtons>
+    }
+}
 
 const styles = StyleSheet.create({});
 
