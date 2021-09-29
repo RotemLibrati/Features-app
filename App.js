67ed2e94a3f42579a4b841c64bf9803a96f5af67
RotemLibrati <rotemlibrati1@gmail.com>
2021-09-29 06:43:48 +0300

    Update react-redux

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PlacesNavigator from './navigation/PlacesNavigator';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import placesReducer from './store/places-reducer';
 
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
   );
 }
 
